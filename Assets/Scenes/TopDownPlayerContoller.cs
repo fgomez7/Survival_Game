9a6e89b36680d758e9eb9119b6cb3f96df17a265
fgomez7 <fgomez7@ucsc.edu>
2025-10-21 23:28:05 -0700

    adding minor changes

--- Assets/Scenes/TopDownPlayerContoller.cs
+++ Assets/Scenes/TopDownPlayerContoller.cs
@@ -1,11 +1,29 @@
 using UnityEngine;
 using UnityEngine.InputSystem; // needed for InputValue
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
 
 public class TopDownPlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;        // movement speed
     private Vector2 moveInput;          // stores WASD input
     private Rigidbody2D rb;             // reference to Rigidbody2D
+    public HealthBar healthBar;
+    public int maxHealth = 100;
+    public int currHealth;
+    public StaminaBar stamina;
+    public int maxStamina = 100;
+    public int currStamina = 100;
+
+    void Start()
+    {
+        currHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
+        currStamina = maxStamina;
+        stamina.SetMaxStamina(maxStamina);
+    }
 
     private void Awake()
     {
@@ -15,12 +33,60 @@ public class TopDownPlayerController : MonoBehaviour
     // This function is automatically called by the PlayerInput component
     void OnMove(InputValue value)
     {
+        //testing damage
         moveInput = value.Get<Vector2>();
+        if ( moveInput.y > 0) //up arrow 
+        {
+            TakeDamage(5);
+        }
+        //if (currStamina > 0 & moveInput != Vector2.zero) {
+        //    useStamina(1);
+        //} else if ( moveInput == Vector2.zero )
+        //{
+        //    refillStamina(2);
+        //}
     }
 
     private void FixedUpdate()
     {
         // Apply movement every physics frame
+        if (currStamina > 0)
+        {
+            rb.velocity = 2 * moveInput * moveSpeed;
+        }
         rb.velocity = moveInput * moveSpeed;
+
+        if ( rb.velocity != Vector2.zero)
+        {
+            useStamina(1);
+        }
+        else if ( rb.velocity == Vector2.zero) 
+        {
+            refillStamina(2);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currHealth -= damage;
+        healthBar.SetHealth(currHealth); 
+    }
+
+    void useStamina(int stam)
+    {
+        if ( currStamina > 0)
+        {
+            currStamina -= stam;
+            stamina.SetStamina(currStamina);
+        }
+    }
+
+    void refillStamina( int stam)
+    {
+        if (currStamina < maxStamina)
+        {
+            currStamina += stam;
+            stamina.SetStamina(currStamina);
+        }
     }
 }
